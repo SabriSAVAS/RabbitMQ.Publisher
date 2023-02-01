c6dae620e8ea594d84f8f11ad29c3c7107d19d94
s.savas <s.savas@SSAVAS-NB>
2023-02-01 14:50:34 +0300

    Add project files.

---
+++ RabbitMQ.Publisher/Program.cs
@@ -0,0 +1,31 @@
+﻿
+
+using RabbitMQ.Client;
+using System.Text;
+
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://smlmmjnn:TlCwjz1TsWxldEMzXgkm_wNOnS5-0g6g@hawk.rmq.cloudamqp.com/smlmmjnn");
+
+//Bağlantuyu Aktifleştirme ve Kanal Açma
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+//Queue Oluşturma
+
+channel.QueueDeclare(queue: "example-queue",exclusive:false);
+
+//Queue'ya Mesaj Gönderme
+
+//RabbitMQ Kuyruğa atacağı mesajları byte türünden kabul etmektedir. Haliyle mesajları bizim byte dönüştürmemiz gerekecektir.
+//byte[] message = Encoding.UTF8.GetBytes("Merhaba");
+//channel.BasicPublish(exchange: "", routingKey: "example-queue", body: message);
+
+for (int i = 0; i < 100; i++)
+{
+    await Task.Delay(200);
+    byte[] message = Encoding.UTF8.GetBytes("Merhaba " + i);
+    channel.BasicPublish(exchange: "", routingKey: "example-queue", body: message);
+}
+
+Console.Read();
